f637081c5fe0afa54edd2561fa6a09a0e3bdd44b
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-24 20:54:14 +0600

    setup google fonts

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,10 +1,11 @@
 import Header from '@/components/shared/Header';
 import Navbar from '@/components/shared/Navbar';
 import React from 'react';
+import { montserrat } from '../layout';
 
 const Authlayout = ({ children }) => {
     return (
-        <div>
+        <div className={`${montserrat.className}`}>
             <Navbar />
             {children}
         </div>
