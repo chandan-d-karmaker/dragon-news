c68d894dec1ba5280730c9e9f994cdcb6a894949
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-25 14:32:31 +0600

    fetched category data and displayed it in section

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,10 +1,25 @@
-import Image from "next/image";
+const getCategories = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+  const data = await res.json();
+  return data.data.news_category;
+}
 
-export default function Home() {
+export default async function Home() {
+
+  const categories = await getCategories();
+
   return (
-    <div className=" grid grid-cols-4 gap-4">
-      <div className="font-bold text-5xl bg-amber-400">
-        All category
+    <div className="grid grid-cols-4 gap-4">
+
+      <div className="">
+        <h2 className="text-xl font-semibold text-[#403F3F] mb-5">All categories</h2>
+       <ul className="flex flex-col gap-4">
+        {
+          categories.map(item => <li key={item.category_id} item={item} className="text-xl bg-[#E7E7E7] p-4 rounded-sm font-medium text-center">
+            {item.category_name}
+          </li>)
+        }
+       </ul>
       </div>
       <div className="font-bold text-5xl bg-red-500 col-span-2">
         All news
